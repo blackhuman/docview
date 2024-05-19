b4e20f095a6c4f33d9fd62ce49e79f055c5657eb
August White <blackhuman01@gmail.com>
2024-05-19 22:44:31 +0800

    [bugfix] sections path

--- src/app/ebookview/[id]/[[...sections]]/page.tsx
+++ src/app/ebookview/[id]/[[...sections]]/page.tsx
@@ -19,6 +19,7 @@ async function loadFile(fileName: string): Promise<string> {
 }
 
 async function loadFileBytes(fileName: string): Promise<File> {
+  fileName = decodeURIComponent(fileName)
   const rootDir = await navigator.storage.getDirectory()
   const fileHandle = await rootDir.getFileHandle(fileName, { create: false })
   return await fileHandle.getFile()
@@ -31,14 +32,14 @@ const Page = () => {
   const renditionRef = useRef<Rendition | null>(null)
   const mountRef = useRef(false)
   const [sectionToc, setSectionToc] = useState<NavItem[]>([])
-  const currentSection = useMemo(() => sections !== undefined ? sections[0] : null, [sections])
+  const currentSection = useMemo(() => sections !== undefined ? sections.join('/') : null, [sections])
 
   useEffect(() => {
     async function load(): Promise<Rendition | null> {
-      console.log('load', mountRef.current)
-      if (mountRef.current === false) return null
+      console.log('load', mountRef.current) 
       const file = await loadFileBytes(fileName)
       const bytes = await file.arrayBuffer()
+      console.log(`file size: ${bytes.byteLength/1000/1000}MB`)
       // @ts-ignore
       const book = new Book(bytes)
       book.loaded.navigation.then(toc => {
@@ -64,24 +65,31 @@ const Page = () => {
         snap: true,
         allowScriptedContent: true
       })
-      rendition.hooks.render.register((view: any) => {
-        view.iframe.referrerPolicy = 'unsafe-url'
-        const document = view.document as Document
-        document.body.style.padding = '0px'
+      rendition.themes.fontSize('140%')
+      rendition.themes.override('padding', '0px')
+      rendition.themes.default({
+        p: {
+          'margin': '10px !important',
+          'line-height': '1.5 !important',
+        }
       })
-      console.log('displayed')
+      rendition.hooks.render.register((view: any) => {
+        // view.iframe.referrerPolicy = 'unsafe-url'
+        // const document = view.document as Document
+        // document.body.style.padding = '0px'
+      })
+      console.log('displayed', currentSection)
       const displayed = currentSection !== null 
         ? rendition.display(currentSection) 
         : rendition.display()
       renditionRef.current = rendition
       return rendition
     }
-    console.log('mount start')
+    console.log('mount start. fileName:', fileName)
     mountRef.current = true
     const renditionLoad = load()
     return () => {
       console.log('unmount')
-      mountRef.current = false
       renditionLoad.then((rendition) => {
         rendition?.destroy()
       })
@@ -90,40 +98,42 @@ const Page = () => {
 
   return (
     <div ref={containerRef} className='w-full h-full flex flex-row'>
-        <div className='basis-1/6 flex flex-col m-3'>
-          <Button onClick={() => router.push('/')}>Home</Button>
-          {
-            <ListboxWrapper>
-              <Listbox aria-label="Actions">
-                {[
-                  (
-                  <ListboxItem key='cover' >
+      <div className='basis-1/6 flex flex-col p-3 h-full'>
+        <div className='w-full'>
+          <Button className='w-full' onClick={() => router.push('/')}>Home</Button>
+        </div>
+        {
+          <ListboxWrapper>
+            <Listbox aria-label="Actions">
+              {[
+                (
+                <ListboxItem key='cover' >
+                  <Link
+                    href={`/ebookview/${fileName}`}>
+                    Cover
+                  </Link>
+                </ListboxItem>
+                ),
+                ...sectionToc.map(sectionItem => (
+                  <ListboxItem key={sectionItem.href} textValue={sectionItem.label} >
                     <Link
-                      href={`/ebookview/${fileName}`}>
-                      Cover
+                      href={`/ebookview/${fileName}/${sectionItem.href}`}>
+                      {sectionItem.label}
                     </Link>
                   </ListboxItem>
-                  ),
-                  ...sectionToc.map(sectionItem => (
-                    <ListboxItem key={sectionItem.href} >
-                      <Link
-                        href={`/ebookview/${fileName}/${sectionItem.href}`}>
-                        {sectionItem.label}
-                      </Link>
-                    </ListboxItem>
-                  ))
-                ]}
-              </Listbox>
-            </ListboxWrapper>
-          }
-        </div>
-        <div id="epub-render" className='w-full h-full'></div>
+                ))
+              ]}
+            </Listbox>
+          </ListboxWrapper>
+        }
+      </div>
+      <div id="epub-render" className='w-full h-full'></div>
     </div>
   )
 }
 
 const ListboxWrapper: React.FC<PropsWithChildren> = ({children}) => (
-  <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100 mt-2">
+  <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100 mt-2 overflow-y-scroll">
     {children}
   </div>
 );
