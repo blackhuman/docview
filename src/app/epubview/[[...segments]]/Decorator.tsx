cd818b619bd4f61c9cca8101386c21639ac83aa6
baiyizhe <baiyizhe@meituan.com>
2024-12-22 18:33:07 +0800

    ignore play files

--- src/app/epubview/[[...segments]]/Decorator.tsx
+++ src/app/epubview/[[...segments]]/Decorator.tsx
@@ -1,6 +1,7 @@
 import '@/app/globals.css'
 import { Manifest } from '@/manifest'
 import { range } from 'es-toolkit'
+import Link from 'next/link'
 
 interface DecoratorProps {
   manifest: Manifest
@@ -19,23 +20,23 @@ export async function Decorator(props: DecoratorProps) {
   const nextPath = nextIndex < manifest.spineFiles.length ? manifest.spineFiles[nextIndex] : null
   return (
     <div className='absolute top-0 left-0 w-0 h-0'>
-      <a className='fixed top-2 left-2' href='/'>Home</a>
+      <Link className='fixed top-2 left-2' href='/'>Home</Link>
       <ul id='toc' className='fixed top-5 left-2 list-none p-0 m-0 flex flex-col items-start overflow-y-scroll toc'>
         {manifest.toc.map(tocItem => {
           const indexRange = range(tocItem.indexRange[0], tocItem.indexRange[1] + 1)
           const firstIndex = indexRange.shift()!
           return (
             <li key={tocItem.index} className='mb-2 flex flex-col items-start'>
-              <a href={`/${basePath}/${manifest.spineFiles[firstIndex]}`} className='text-blue-500 hover:text-blue-700'>
+              <Link href={`/${basePath}/${manifest.spineFiles[firstIndex]}`} className='text-blue-500 hover:text-blue-700'>
                 {tocItem.name}
-              </a>
+              </Link>
               {indexRange.length > 0 && (
                 <ul className='list-none p-0 ml-2 flex flex-col items-start'>
                   {indexRange.map(index => (
                     <li key={index}>
-                      <a href={`/${basePath}/${manifest.spineFiles[index]}`} className='text-blue-500 hover:text-blue-700'>
+                      <Link href={`/${basePath}/${manifest.spineFiles[index]}`} className='text-blue-500 hover:text-blue-700'>
                         {manifest.spineFiles[index].split('/').pop()}
-                      </a>
+                      </Link>
                     </li>
                   ))}
                 </ul>
